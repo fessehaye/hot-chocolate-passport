cd0289328c850c514209369c0db2558b8e3d52c3
fessehaye <fessehay@ualberta.ca>
2024-01-19 17:44:23 -0800

    Add home link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,9 +4,11 @@ const Header: React.FC = () => {
 
   return (
     <div className="w-full mb-4">
-      <h2 className="text-2xl font-bold tracking-tight">
-        Hot Chocolate Passport
-      </h2>
+      <a href="/" className="no-underline hover:text-red-700 transition text-black">
+        <h1 className="text-2xl font-bold tracking-tight">
+          Hot Chocolate Passport
+        </h1>
+      </a>
       <p className="my-2">
         Here's a list of hot chocolate drinks for this year's festival. 
         You can visit the original site for more information <a href="https://hotchocolatefest.com/list-of-flavours">here</a>!
