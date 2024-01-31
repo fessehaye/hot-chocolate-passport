b148ebdf94e987015d8a92d15084b0b8e889a63b
fessehaye <fessehay@ualberta.ca>
2024-01-31 11:21:16 -0800

    Description

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
         You can visit the original site for more information <a href="https://hotchocolatefest.com/list-of-flavours">here</a>!
       </p>
       <p className="my-2">You can mark the drinks you've tried and save your favorites.</p>
-      <p className="my-2">Remember to always check the original site and stores for accuracy.</p>
+      <p className="my-2">Remember to always check the original site and stores for accuracy. The map from the original site is also added when you click on the map icon.</p>
     </div>
   );
 };
